3c6b8bfc62900177e64253e284c44a0744ad788b
weijie-jin <weijiejin0830@163.com>
2019-11-03 16:15:39 +0800

    多线程通信--问题引入

---
+++ Xian14/src/Xian14.java
@@ -0,0 +1,14 @@
+
+public class Xian14 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Storage st = new Storage();
+		Input input = new Input(st);
+		Output output = new Output(st);
+		new Thread(input).start();
+		new Thread(output).start();
+
+	}
+
+}
