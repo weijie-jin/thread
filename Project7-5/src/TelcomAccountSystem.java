f0464d76569f7ea14dc6e5ea8eef7bcbcb5709c1
weijie-jin <weijiejin0830@163.com>
2019-11-24 15:54:43 +0800

    HashSet去重无序

---
+++ Project7-5/src/TelcomAccountSystem.java
@@ -0,0 +1,20 @@
+
+public class TelcomAccountSystem {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//实例化一个电信用户类TelcomUser
+				TelcomUserBasedOnCollection telcomUser = new TelcomUserBasedOnCollection("13800138000");
+			   
+				//生成通话记录
+				telcomUser.generateCommunicateRecord();
+				//打印通话详单
+				telcomUser.printDetails();
+				//打印HashSet集合中的去重、无序元素
+				telcomUser.printcallToNumbers();
+				
+
+	}
+
+}
